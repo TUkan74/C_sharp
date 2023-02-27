2b788ecede6638dd2c40cac14ba1a18c386ea445
TUkan74 <112977455+TUkan74@users.noreply.github.com>
2023-02-27 09:32:50 +0100

    Add files via upload

---
+++ Factorilazation/Factorilazation/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System;
+class Hello
+{
+    static string Fact(int num)
+    {
+        string vysledok = $"{num}"+"=";
+        int i = 2;
+        while (i*i <= num) 
+        {
+            if (num%i != 0 )
+            {
+                i++;
+            }
+            else
+            {
+                num/=i;
+                vysledok += $"{i}" + "*";
+            }
+
+        }
+        if (num>1)
+        {
+            vysledok += $"{num}";
+        }
+        return vysledok;
+         
+    }
+    
+    
+    static void Main(string[] args)
+    {
+        // Output: Hello, world!
+        int cislo = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(Fact(cislo));
+    }
+}
+

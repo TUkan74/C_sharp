bb056ed75e612d3a9df828bc6fc2ba0e464601da
Matus <klecka.matus@gmail.com>
2023-03-02 15:52:25 +0100

    variables

--- knight/Knight_path/Knight_path/Program.cs
+++ knight/Knight_path/Knight_path/Program.cs
@@ -32,35 +32,58 @@ namespace Knight_path
             //variables
             int[,] board;
             board = new int[8,8];
-
-
+            (int, int,int) sur;
+            (int, int,int) start;
+            (int, int) end;
+            string[] cor;
+            Queue<(int,int,int)> Q = new Queue<(int,int,int)>();
 
 
             //get input
-            (int, int) sur = (0, 0);
-            (int , int) start = (0, 0);
-            (int, int) end = (0, 0);
-            string[] cor;
+
             int num_o = Convert.ToInt32(Console.ReadLine()) ;
             //coordinance of obstacles added to board
             for (int i = 0;i < num_o;i++) {
                 
                 cor=(Console.ReadLine()).Split(' ');
-                board[Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1])] = 1;
+                board[Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1])] = 99;
             }
+            
             //start position
             cor = (Console.ReadLine()).Split(' ');
-            start = (Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1]));
+            start = (Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1]),0);
+            board[Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1])] = 0;
+
             //end position
             cor = (Console.ReadLine()).Split(' ');
             end = (Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1]));
+            board[Convert.ToInt32(cor[0]), Convert.ToInt32(cor[1])] = -1;
+
+            Console.WriteLine($"{start}");
+
+            Q.Enqueue(start);
+            (int,int,int) pos; 
+            (int, int, int) next;
+            while (Q.Count > 0)
+            {
+                pos =  Q.Dequeue();
+
+            }
+
+            
+
+
+
+
+
+
 
             //write out whole array
             /*foreach (var sq in sachovnica)
             {
                 System.Console.WriteLine($"<{sq}>");
             }*/
-            
+
             Console.ReadLine();
         }
     }

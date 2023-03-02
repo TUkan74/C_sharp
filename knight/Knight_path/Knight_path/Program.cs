7b5be0be41bca1f03d46f7844780654d9dbd57d7
Matus <klecka.matus@gmail.com>
2023-03-02 13:02:20 +0100

    is valid_funcion

--- knight/Knight_path/Knight_path/Program.cs
+++ knight/Knight_path/Knight_path/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Diagnostics.SymbolStore;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,14 +11,46 @@ namespace Knight_path
 {
     internal class Program
     {
+        //can you jump there? 
+        bool is_valid(int x, int y,bool[,] sachovnica)
+        {
+            if ((0 <= x) && (x < 8))
+            {
+                if ((0 <= y) && (y < 8))
+                {
+                    if (sachovnica[x,y] == false)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return  true;
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            //variables
+            bool[,] sachovnica;
+            sachovnica = new bool[8,8];
+
+
+
+
+            //get input
+            (int, int) sur = (0, 0);
+            (int , int) start = (0, 0);
+            (int, int) end = (0, 0);
+            string cor;
+            int num_o = Convert.ToInt32(Console.ReadLine()) ;
             
-            string coor =  Console.ReadLine();
-
-            Console.WriteLine(coor);
-
+            for (int i = 0;i < num_o;i++)
+            {
+                cor = Console.ReadLine();
+                for (int j = 0;j < cor.Length;j++) {
+                    if (cor[j] != )
+                }
+            }
+            //Console.WriteLine($"{sachovnica[0, 0]}");
             Console.ReadLine();
         }
     }

b60c8e31afb2f36cfbeb1b6e74f4d43f299b274d
Matus <klecka.matus@gmail.com>
2023-03-06 10:24:06 +0100

    starting classes

--- Chess_danger/Chess/Chess/Program.cs
+++ Chess_danger/Chess/Chess/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Chess;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+
+    class Figure
+    {
+
+        public (int x, int y) GetNext()
+        {
+
+        }
+    }
+
+
+    class Bishop : Figure
+    {
+        public (int x, int y) GetNext()
+        {
+            return (1, 2);
+        }
+    }
+
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Bishop bishop = new Bishop();
+            Console.WriteLine(b.GetNext());
+        }
+    }
+}
